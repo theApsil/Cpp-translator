a4a67b20149d525ad1cacdae4ba784f40e1bae39
Danil Goncharuk <goncharuk.dm@students.dvfu.ru>
2023-11-30 11:03:53 +1000

    Parser finished

--- Parser/ParseGrammar.py
+++ Parser/ParseGrammar.py
@@ -28,13 +28,45 @@ class ParserGrammar:
         return None
 
     def parseTxtRulesNames(self, file):
-        # Finish this
-        return None
+        file.seek(0)
+        result = []
+        lineNumber = 0
+        for line in file.readlines():
+            lineNumber += 1
+            if bool(re.search('^<.+>->.+$', line)):
+                marchingResult = re.search('^(<.+>)->', line)
+                result.append(marchingResult.group(1))
+            else:
+                self.errors.append(GrammarParserError(lineNumber, GrammarParserErrorType.INVALID_RULE_STRUCT))
+        for rule in result:
+            self.rules.append(Rule(rule))
+
 
     def parseTxtRulesProductions(self, file):
-        # Finish this
-        return None
+        file.seek(0)
+        lineNumber = 0
+        for line in file.readlines():
+            lineNumber += 1
+            if bool(re.search('^<.+>->.+$', line)):
+                marchingResult = re.search('^(<.+>)->', line)
+                ruleName = marchingResult.group(1)
+                rightSide = line.replace('\n', '').split('->')[1].split('|')
+
+                rule = self.searchRule(ruleName)
+                if rule:
+                    for production in rightSide:
+                        prodRule = self.searchRule(production)
+                        if prodRule:
+                            rule.add(prodRule)
+                        else:
+                            rule.add(production)
+                else:
+                    self.errors.append(GrammarParserError(lineNumber, GrammarParserErrorType.INVALID_RULE_NAME))
+
+            else:
+                self.errors.append(GrammarParserError(lineNumber, GrammarParserErrorType.INVALID_RULE_STRUCT))
     
+
     def parseTxtRules(self, filePath):
         file = open(filePath, 'r', encoding="utf-8")
         self.parseTxtRulesNames(file)
@@ -48,5 +80,32 @@ class ParserGrammar:
             print(repr(rule))
 
     def parseJsonRules(self, filePath):
-        # Finish this
-        return None
+        with open(filePath, 'r', encoding="utf-8") as jsonFile:
+            sourceJson = json.load(jsonFile)
+
+            for i, rule in enumerate(sourceJson):
+                ruleName = rule['ruleName']
+                if self.searchRule(ruleName) is None:
+                    self.rules.append(Rule(ruleName))
+                else:
+                    self.errors.append(GrammarParserError(i, GrammarParserErrorType.REPEATED_RULE_NAME))
+
+            if self.errors:
+                ValueError("Invalid grammar")
+
+            for i, rule in enumerate(sourceJson):
+                workingRule = self.searchRule(rule['ruleName'])
+                productions = rule["productions"]
+                for production in productions:
+                    prodAccum = Production()
+                    for elem in production:
+                        existRule = self.searchRule(elem)
+                        if existRule:
+                            prodAccum.add(existRule)
+                        else:
+                            match = re.search('reg{(.+)}', elem)
+                            if match:
+                                prodAccum.add(RegexpRule(match.group(1)))
+                            else:
+                                prodAccum.add(elem)
+                    workingRule.add(prodAccum)
